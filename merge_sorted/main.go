99b03b163a84ef652e299776c5799b3ab05153bf
Minh Nguyen <minhnguyen@Minhs-MacBook-Pro-2.local>
2025-01-03 22:01:45 -0800

    work in progress

--- merge_sorted/main.go
+++ merge_sorted/main.go
@@ -4,6 +4,45 @@ import "fmt"
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	fmt.Println("Calling merge")
+	s2 := 0
+	tmp := 0
+
+	/*
+		Need to handle with nums1 is empty
+		Need to think about if nums2 is always smaller
+		Need to think about when nums1 is always smaller
+	*/
+
+	if n == 0 {
+		return
+	}
+
+	if m == 0 {
+		nums1 = nums2
+		return
+	}
+
+	for i, val := range nums1 {
+		fmt.Println(i, val)
+
+		if tmp > nums2[s2] && s2 < n {
+			if val > nums2[s2] {
+				tmp = nums1[i]
+				nums1[i] = nums2[s2]
+				nums2[s2] = tmp
+			}
+		} else if s2 < n {
+			if val > nums2[s2] {
+				tmp = val
+				nums[i] = nums2[s2]
+				s2 += 1
+			} else if val > tmp && tmp != 0 {
+
+			}
+		} else {
+
+		}
+	}
 }
 
 func main() {
