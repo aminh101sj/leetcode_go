f377f539c48ec327b0fd6d992d27235e48a5fbaa
Minh Nguyen <minhnguyen@Minhs-MacBook-Pro-2.local>
2025-01-05 15:09:17 -0800

    Algorithm to change in place

--- merge_sorted/main.go
+++ merge_sorted/main.go
@@ -4,8 +4,7 @@ import "fmt"
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	fmt.Println("Calling merge")
-	s2 := 0
-	tmp := 0
+	nums2Index := 0
 
 	/*
 		Need to handle with nums1 is empty
@@ -13,39 +12,39 @@ func merge(nums1 []int, m int, nums2 []int, n int) {
 		Need to think about when nums1 is always smaller
 	*/
 
-	if n == 0 {
+	// nothing to add from nums2 or both is empty
+	if n == 0 || m == 0 {
 		return
 	}
 
-	if m == 0 {
-		nums1 = nums2
-		return
+	tmpNums := make([]int, m)
+	for i := 0; i < m; i++ {
+		tmpNums[i] = nums1[i]
 	}
+	fmt.Println(tmpNums)
 
-	for i, val := range nums1 {
-		fmt.Println(i, val)
-
-		if tmp > nums2[s2] && s2 < n {
-			if val > nums2[s2] {
-				tmp = nums1[i]
-				nums1[i] = nums2[s2]
-				nums2[s2] = tmp
-			}
-		} else if s2 < n {
-			if val > nums2[s2] {
-				tmp = val
-				nums[i] = nums2[s2]
-				s2 += 1
-			} else if val > tmp && tmp != 0 {
-
-			}
+	nums1Index := 0
+	for i, _ := range nums1 {
+		if nums2Index >= n {
+			continue
+		} else if nums1Index >= m {
+			nums1[i] = nums2[nums2Index]
+			nums2Index += 1
+		} else if tmpNums[nums1Index] > nums2[nums2Index] {
+			nums1[i] = nums2[nums2Index]
+			nums2Index += 1
 		} else {
-
+			nums1[i] = tmpNums[nums1Index]
+			nums1Index += 1
 		}
 	}
+	fmt.Println(nums1)
 }
 
 func main() {
 	fmt.Println("Hello World")
-	merge([]int{1, 2, 3, 0, 0, 0}, 3, []int{2, 5, 6}, 3)
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	nums2 := []int{2, 5, 6}
+	merge(nums1, 3, nums2, 3)
+	fmt.Println(nums1)
 }
